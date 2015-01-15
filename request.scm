c513fae4e3c570673b1fa23e8ea384e5818762b9
Richo Healey <richo@psych0tik.net>
2015-01-14 17:51:34 -0800

    request: Migrate to short define syntax

--- request.scm
+++ request.scm
@@ -1,32 +1,27 @@
-(define make-request
-  (lambda (port)
+(define (make-request port)
     (let ((first-line (read-line port)))
       (if (equal? first-line #!eof)
         #f
         `((path . ,(parse-request-path first-line))
           (method . ,(parse-request-method first-line))
           (headers . ,(parse-request-headers port))
-          (body-port . ,port))))))
+          (body-port . ,port)))))
 
-(define parse-request-method
-  (lambda (line)
-    (car (string-tokenize line))))
+(define (parse-request-method line)
+    (car (string-tokenize line)))
 
-(define parse-request-path
+(define (parse-request-path line)
   ; Maximum ghetto. Expect a line like:
   ; GET /index.html HTTP/1.1
-  (lambda (line)
     (let ((tokenized-line (string-tokenize line)))
       ; TODO strip query strings
-      (cadr tokenized-line))))
+      (cadr tokenized-line)))
 
-(define parse-header
-  (lambda (line)
+(define (parse-header line)
     (let ((index (string-index line #\:)))
-      `(,(string-upcase (substring line 0 index)) . ,(string-trim (substring line (+ index 1)))))))
+      `(,(string-upcase (substring line 0 index)) . ,(string-trim (substring line (+ index 1))))))
 
-(define parse-request-headers
-  (lambda (port)
+(define (parse-request-headers port)
     (call/cc (lambda (return)
       (letrec ((loop (lambda (current)
         (let ((this-line (read-line port)))
@@ -40,15 +35,14 @@
                      (car (parse-header this-line))
                      (cdr (parse-header this-line))
                      current))))))))
-        (loop '()))))))
+        (loop '())))))
 
-(define make-fetcher
-  (lambda (key)
+(define (make-fetcher key)
     (lambda (request)
       (let ((val (assoc key request)))
         (if key
           (cdr val)
-          #f)))))
+          #f))))
 
 (define get-request-method    (make-fetcher 'method))
 (define get-request-path      (make-fetcher 'path))
@@ -58,10 +52,9 @@
                                 (let ((bytes (get-request-header request "Content-Length")))
                                   (read-string (if bytes (string->number bytes) #f) (get-request-body-port request)))))
 
-(define get-request-header
-  (lambda (request header)
+(define (get-request-header request header)
     (let* ((headers (get-request-headers request))
            (val (assoc (string-upcase header) headers)))
       (if val
         (cdr val)
-        #f))))
+        #f)))
