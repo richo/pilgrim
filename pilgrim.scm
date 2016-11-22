d2e324eef93011383393266be7454d73cb1b16a2
Daniel Ziltener <dziltener@lyrion.ch>
2016-11-22 22:38:11 +0100

    Turned it into a module.

--- pilgrim.scm
+++ pilgrim.scm
@@ -1,31 +1,35 @@
-(use srfi-13)
-(use srfi-18)
-(use tcp)
-(use posix)
-(use utils)
+(module pilgrim
+    (export start make-request)
+  (import scheme)
+  (import chicken)
+  (import extras)
+  (import data-structures)
+  (use srfi-13)
+  (use srfi-18)
+  (use tcp)
+  (use posix)
+  (use utils)
 
-(load-relative "response.scm")
-(load-relative "request.scm")
+  (include "response")
+  (include "request")
 
-(define (handle in out handler)
-    ; We're lazy- we can find out everything about the request that we care
-    ; about from it's first line
+  (define (handle in out handler)
     (let ((request (make-request in))
           (response (make-response)))
       (if request
-        (write-response
-          (handler request response)
-          out))
+	  (write-response
+	   (handler request response)
+	   out))
       (close-input-port in)
       (close-output-port out)))
 
-(define (handle-in-thread in out handler)
+  (define (handle-in-thread in out handler)
     (thread-start! (make-thread (lambda () (handle in out handler)))))
 
-(define (start port threaded? handler)
+  (define (start port threaded? handler)
     (letrec ((sock (tcp-listen port))
-      (mainloop (lambda ()
-        (let-values (((s-in s-out) (tcp-accept sock)))
-          ((if threaded? handle-in-thread handle) s-in s-out handler)
-          (mainloop)))))
-      (mainloop)))
+	     (mainloop (lambda ()
+			 (let-values (((s-in s-out) (tcp-accept sock)))
+			   ((if threaded? handle-in-thread handle) s-in s-out handler)
+			   (mainloop)))))
+      (mainloop))))
